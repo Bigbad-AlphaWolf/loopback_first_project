0a08f8cb921d151db76e37761def0f6845c039e3
Mohamed Abdoul Karim Diop <mohamedabdoulkarim.diop2@orange-sonatel.com>
2021-04-28 23:21:27 +0000

    Integration jwt from tuto loopback__in_progress

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,4 @@ export * from './ping.controller';
 export * from './hello.controller';
 export * from './todo.controller';
 export * from './mail-sender.controller';
+export * from './user.controller';
